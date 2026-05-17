fde57d4a35624877c3c275df39de3c628e100136
andresnico44 <ramiroandresnc@ufps.edu.co>
2026-05-17 02:36:54 -0500

    fix(smtp): change default email port to 2525 to bypass railway firewall

--- core/settings.py
+++ core/settings.py
@@ -155,7 +155,7 @@ SIMPLE_JWT = {
 # Configuración del Servidor de Correos (Brevo SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 2525))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
