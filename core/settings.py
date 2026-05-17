f8ff20abf30b8338a58a4aeab0f8d6378a180270
andresnico44 <ramiroandresnc@ufps.edu.co>
2026-05-17 12:14:49 -0500

    fix(settings): load BREVO_API_KEY from environment variables

--- core/settings.py
+++ core/settings.py
@@ -160,3 +160,4 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'no-reply@sig-arroz.com')
+BREVO_API_KEY = os.getenv('BREVO_API_KEY')
